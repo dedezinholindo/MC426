e5d0cc2c8e2bdb401dcef05c918b123593ca34f4
Andre Ricardo <dedaricardo@gmail.com>
2023-11-20 13:57:39 -0300

    test edit profile added

--- backend/profile/test_edit_profile.py
+++ backend/profile/test_edit_profile.py
@@ -1,78 +1,134 @@
-import unittest
-import json
-import tempfile
-from edit_profile import app, write_users_to_file
+from flask import Flask, request, render_template_string, escape
 
-class TestFlaskApp(unittest.TestCase):
-    def setUp(self):
-        app.config['TESTING'] = True
-        self.app = app.test_client()
+app = Flask(__name__)
 
-        # Create a temporary file with mock user data
-        self.tempfile = tempfile.NamedTemporaryFile(mode='w', delete=False)
-        mock_users = [
-            {
-                "id": "1",
-                "username": "test_user1",
-                "password": "test_password1",
-                "name": "Test User 1",
-                "age": "30",
-                "email": "test_user1@example.com"
-            },
-            {
-                "id": "2",
-                "username": "test_user2",
-                "password": "test_password2",
-                "name": "Test User 2",
-                "age": "25",
-                "email": "test_user2@example.com"
-            },
-            {
-                "id": "3",
-                "username": "test_user3",
-                "password": "test_password3",
-                "name": "Test User 3",
-                "age": "40",
-                "email": "test_user3@example.com"
-            }
-        ]
-        write_users_to_file(mock_users)
+@app.route('/')
+def index():
+    return """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Seu Site</title>
+        <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='styles.css') }}">
+    </head>
+    <body>
+        <div class="navbar">
+            <div class="container">
+                <h1>Seu Site</h1>
+                <ul>
+                    <li><a href="/">Página Inicial</a></li>
+                    <li><a href="/xss">XSS</a></li>
+                    <li><a href="/injection">Injeção</a></li>
+                    <li><a href="/auth">Autenticação</a></li>
+                    <li><a href="/directory_traversal">Travessia de Diretório</a></li>
+                    <li><a href="/insecure_deserialization">Deserialização Insegura</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="container">
+            <h2>Página Inicial</h2>
+            <p>Bem-vindo ao nosso site!</p>
+        </div>
+    </body>
+    </html>
+    """
 
-    def tearDown(self):
-        # Close and remove the temporary file
-        self.tempfile.close()
+@app.route('/xss')
+def xss():
+    input_usuario = request.args.get('input')
+    input_usuario = escape(input_usuario)  # Escapa qualquer entrada do usuário para evitar XSS
+    template = f"<p>{input_usuario}</p>"
+    return template
 
-    def test_edit_profile(self):
-        # Test case 1: Edit profile successfully
-        response = self.app.put('/edit_profile', json={"username": "test_user1", "name": "New Name", "age": "25", "email": "new@email.com"})
-        print(response.get_data(as_text=True))
-        data = json.loads(response.get_data(as_text=True))
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(data["message"], "Profile updated successfully")
+@app.route('/injection')
+def injection():
+    user_input = request.args.get('input')
+    try:
+        result = eval(user_input)
+        return f"Resultado: {result}"
+    except Exception as e:
+        return f"Erro: {str(e)}"
 
-        # Test case 2: User not found
-        response = self.app.put('/edit_profile', json={"username": "non_existing_user", "name": "New Name", "age": "25", "email": "new@email.com"})
-        data = json.loads(response.get_data(as_text=True))
-        self.assertEqual(response.status_code, 404)
-        self.assertEqual(data["message"], "User not found")
+@app.route('/auth', methods=['GET', 'POST'])
+def auth():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
 
-        # Test case 3: Invalid request (missing fields)
-        response = self.app.put('/edit_profile', json={"name": "New Name", "age": "25"})
-        data = json.loads(response.get_data(as_text=True))
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(data["message"], "Missing required fields")
+        if username == 'admin' and password == 'admin123':
+            return "Autenticado como admin"
+        else:
+            return "Falha na autenticação"
 
-        # Test case 4: Edit profile for another user successfully
-        response = self.app.put('/edit_profile', json={"username": "test_user2", "name": "Updated Name", "age": "28", "email": "updated@email.com"})
-        data = json.loads(response.get_data(as_text=True))
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(data["message"], "Profile updated successfully")
+    return '''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Autenticação</title>
+        <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='styles.css') }}">
+    </head>
+    <body>
+        <div class="container">
+            <h2>Autenticação</h2>
+            <form method="post">
+                <input type="text" name="username" placeholder="Usuário"><br>
+                <input type="password" name="password" placeholder="Senha"><br>
+                <input type="submit" value="Login">
+            </form>
+        </div>
+    </body>
+    </html>
+    '''
 
-        # Test case 5: Edit profile with non-numeric age
-        response = self.app.put('/edit_profile', json={"username": "test_user3", "name": "Updated Name", "age": "Invalid", "email": "updated@email.com"})
-        data = json.loads(response.get_data(as_text=True))
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(data["message"], "Age must contain only numbers")
+@app.route('/directory_traversal')
+def directory_traversal():
+    # Adicione verificação de caminho de arquivo seguro aqui
+    file_path = request.args.get('file')
+    if ".." in file_path:
+        return "Acesso não autorizado"
+    
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+        return content
+    except FileNotFoundError:
+        return "Arquivo não encontrado"
+
+@app.route('/insecure_deserialization', methods=['POST', 'GET'])
+def insecure_deserialization():
+    if request.method == 'POST':
+        import pickle
+        serialized_data = request.form.get('data')
+        try:
+            obj = pickle.loads(serialized_data)
+            return f"Deserializado: {obj}"
+        except Exception as e:
+            return f"Erro ao desserializar: {str(e)}"
+
+    return '''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Deserialização Insegura</title>
+        <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='styles.css') }}">
+    </head>
+    <body>
+        <div class="container">
+            <h2>Deserialização Insegura</h2>
+            <form method="post">
+                <textarea name="data" placeholder="Dados serializados"></textarea><br>
+                <input type="submit" value="Deserializar">
+            </form>
+        </div>
+    </body>
+    </html>
+    '''
 
 if __name__ == '__main__':
-    unittest.main()
+    app.run(debug=True)
