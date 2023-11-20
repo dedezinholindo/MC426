6989cc25fe53be0e7e44580dd96c46b8e402b7df
Andre Ricardo <dedaricardo@gmail.com>
2023-11-20 15:55:55 -0300

    added fields to the edit profile and edited the test

--- backend/profile/edit_profile.py
+++ backend/profile/edit_profile.py
@@ -28,22 +28,49 @@ def edit_profile():
     new_name = user_data.get("name")
     new_age = user_data.get("age")
     new_email = user_data.get("email")
+    new_phone = user_data.get("phone")
+    new_address = user_data.get("address")
+    new_photo = user_data.get("photo")
+    new_safety_number = user_data.get("safetyNumber")
 
-    if not (username and new_name and new_age and new_email):
+    if not (username and new_name and new_age and new_email and new_phone and new_address and new_photo and new_safety_number):
         return jsonify({"message": "Missing required fields"}), 400
+    
+    
+    # Check specific criteria for each field
+    if not (1 <= len(new_name) <= 50):
+        return jsonify({"message": "Name must be between 1 and 50 characters"}), 400
 
-    users = read_users_from_file()
+    if not new_email or '@' not in new_email or '.' not in new_email:
+        return jsonify({"message": "Invalid email address"}), 400
 
-    # Check if the username exists
+    if not new_age.isdigit() or not (1 <= int(new_age) <= 150):
+        return jsonify({"message": "Age must be a number between 1 and 150"}), 400
+
+    if not new_phone.isdigit() or len(new_phone) != 11:
+        return jsonify({"message": "Phone number must be an 11-digit number"}), 400
+
+    if not new_safety_number.isdigit() or len(new_safety_number) != 11:
+        return jsonify({"message": "Safety number must be an 11-digit number"}), 400
+
+    if not new_address:
+        return jsonify({"message": "Address is required"}), 400
+
+    users = read_users_from_file() #change for database
+
     for user in users:
         if user['username'] == username:
             # Update user information
             user['name'] = new_name
             user['age'] = new_age
             user['email'] = new_email
+            user['phone'] = new_phone
+            user['address'] = new_address
+            user['photo'] = new_photo
+            user['safetyNumber'] = new_safety_number
             
-            if not new_age.isdigit():
-                return jsonify({"message": "Age must contain only numbers"}), 400
+            if not (new_age.isdigit() and new_phone.isdigit() and len(new_phone) == 11 and len(new_safety_number) == 11):
+                return jsonify({"message": "Invalid age, phone, or safetyNumber format"}), 400
 
             # Write updated user information to file
             write_users_to_file(users)
--- backend/profile/test_edit_profile.py
+++ backend/profile/test_edit_profile.py
@@ -1,134 +1,188 @@
-from flask import Flask, request, render_template_string, escape
+import unittest
+import json
+import tempfile
+from edit_profile import app, write_users_to_file
 
-app = Flask(__name__)
+class TestFlaskApp(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
 
-@app.route('/')
-def index():
-    return """
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Seu Site</title>
-        <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='styles.css') }}">
-    </head>
-    <body>
-        <div class="navbar">
-            <div class="container">
-                <h1>Seu Site</h1>
-                <ul>
-                    <li><a href="/">Página Inicial</a></li>
-                    <li><a href="/xss">XSS</a></li>
-                    <li><a href="/injection">Injeção</a></li>
-                    <li><a href="/auth">Autenticação</a></li>
-                    <li><a href="/directory_traversal">Travessia de Diretório</a></li>
-                    <li><a href="/insecure_deserialization">Deserialização Insegura</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="container">
-            <h2>Página Inicial</h2>
-            <p>Bem-vindo ao nosso site!</p>
-        </div>
-    </body>
-    </html>
-    """
+        # Create a temporary file with mock user data
+        self.tempfile = tempfile.NamedTemporaryFile(mode='w', delete=False)
+        mock_users = [
+            {
+                "id": "1",
+                "username": "test_user1",
+                "password": "test_password1",
+                "name": "Test User 1",
+                "age": "30",
+                "email": "test_user1@example.com",
+                "phone": "12345678901",
+                "address": "Test Address 1",
+                "photo": "test1.jpg",
+                "safetyNumber": "12345678901"
+            },
+            {
+                "id": "2",
+                "username": "test_user2",
+                "password": "test_password2",
+                "name": "Test User 2",
+                "age": "25",
+                "email": "test_user2@example.com",
+                "phone": "23456789012",
+                "address": "Test Address 2",
+                "photo": "test2.jpg",
+                "safetyNumber": "23456789012"
+            },
+            {
+                "id": "3",
+                "username": "test_user3",
+                "password": "test_password3",
+                "name": "Test User 3",
+                "age": "40",
+                "email": "test_user3@example.com",
+                "phone": "34567890123",
+                "address": "Test Address 3",
+                "photo": "test3.jpg",
+                "safetyNumber": "34567890123"
+            }
+        ]
+        write_users_to_file(mock_users)
 
-@app.route('/xss')
-def xss():
-    input_usuario = request.args.get('input')
-    input_usuario = escape(input_usuario)  # Escapa qualquer entrada do usuário para evitar XSS
-    template = f"<p>{input_usuario}</p>"
-    return template
+    def tearDown(self):
+        # Close and remove the temporary file
+        self.tempfile.close()
 
-@app.route('/injection')
-def injection():
-    user_input = request.args.get('input')
-    try:
-        result = eval(user_input)
-        return f"Resultado: {result}"
-    except Exception as e:
-        return f"Erro: {str(e)}"
+    def test_edit_profile(self):
+        # Test case 1: Edit profile successfully
+        response = self.app.put('/edit_profile', json={
+            "username": "test_user1",
+            "name": "New Name",
+            "age": "25",
+            "email": "new@email.com",
+            "phone": "12345678901",
+            "address": "New Address",
+            "photo": "new.jpg",
+            "safetyNumber": "12345678901"
+        })
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["message"], "Profile updated successfully")
 
-@app.route('/auth', methods=['GET', 'POST'])
-def auth():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+        # Test case 2: User not found
+        response = self.app.put('/edit_profile', json={
+            "username": "non_existing_user",
+            "name": "New Name",
+            "age": "25",
+            "email": "new@email.com",
+            "phone": "12345678901",
+            "address": "New Address",
+            "photo": "new.jpg",
+            "safetyNumber": "12345678901"
+        })
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data["message"], "User not found")
 
-        if username == 'admin' and password == 'admin123':
-            return "Autenticado como admin"
-        else:
-            return "Falha na autenticação"
+        # Test case 3: Invalid request (missing fields)
+        response = self.app.put('/edit_profile', json={
+            "name": "New Name",
+            "age": "25"
+        })
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data["message"], "Missing required fields")
 
-    return '''
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Autenticação</title>
-        <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='styles.css') }}">
-    </head>
-    <body>
-        <div class="container">
-            <h2>Autenticação</h2>
-            <form method="post">
-                <input type="text" name="username" placeholder="Usuário"><br>
-                <input type="password" name="password" placeholder="Senha"><br>
-                <input type="submit" value="Login">
-            </form>
-        </div>
-    </body>
-    </html>
-    '''
+        # Test case 4: Edit profile for another user successfully
+        response = self.app.put('/edit_profile', json={
+            "username": "test_user2",
+            "name": "Updated Name",
+            "age": "28",
+            "email": "updated@email.com",
+            "phone": "23456789012",
+            "address": "Updated Address",
+            "photo": "updated.jpg",
+            "safetyNumber": "23456789012"
+        })
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["message"], "Profile updated successfully")
 
-@app.route('/directory_traversal')
-def directory_traversal():
-    # Adicione verificação de caminho de arquivo seguro aqui
-    file_path = request.args.get('file')
-    if ".." in file_path:
-        return "Acesso não autorizado"
-    
-    try:
-        with open(file_path, 'r') as file:
-            content = file.read()
-        return content
-    except FileNotFoundError:
-        return "Arquivo não encontrado"
+        # Test case 5: Edit profile with non-numeric age
+        response = self.app.put('/edit_profile', json={
+            "username": "test_user3",
+            "name": "Updated Name",
+            "age": "Invalid",
+            "email": "updated@email.com",
+            "phone": "34567890123",
+            "address": "Updated Address",
+            "photo": "updated.jpg",
+            "safetyNumber": "34567890123"
+        })
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data["message"], "Age must be a number between 1 and 150")
 
-@app.route('/insecure_deserialization', methods=['POST', 'GET'])
-def insecure_deserialization():
-    if request.method == 'POST':
-        import pickle
-        serialized_data = request.form.get('data')
-        try:
-            obj = pickle.loads(serialized_data)
-            return f"Deserializado: {obj}"
-        except Exception as e:
-            return f"Erro ao desserializar: {str(e)}"
+        # Test case 6: Edit profile with non-numeric phone
+        response = self.app.put('/edit_profile', json={
+            "username": "test_user1",
+            "name": "Updated Name",
+            "age": "25",
+            "email": "updated@email.com",
+            "phone": "Invalid",
+            "address": "Updated Address",
+            "photo": "updated.jpg",
+            "safetyNumber": "12345678901"
+        })
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data["message"], "Phone number must be an 11-digit number")
 
-    return '''
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Deserialização Insegura</title>
-        <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='styles.css') }}">
-    </head>
-    <body>
-        <div class="container">
-            <h2>Deserialização Insegura</h2>
-            <form method="post">
-                <textarea name="data" placeholder="Dados serializados"></textarea><br>
-                <input type="submit" value="Deserializar">
-            </form>
-        </div>
-    </body>
-    </html>
-    '''
+        # Test case 7: Edit profile with invalid phone length
+        response = self.app.put('/edit_profile', json={
+            "username": "test_user1",
+            "name": "Updated Name",
+            "age": "25",
+            "email": "updated@email.com",
+            "phone": "123",
+            "address": "Updated Address",
+            "photo": "updated.jpg",
+            "safetyNumber": "12345678901"
+        })
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data["message"], "Phone number must be an 11-digit number")
+
+        # Test case 8: Edit profile with non-numeric safetyNumber
+        response = self.app.put('/edit_profile', json={
+            "username": "test_user1",
+            "name": "Updated Name",
+            "age": "25",
+            "email": "updated@email.com",
+            "phone": "12345678901",
+            "address": "Updated Address",
+            "photo": "updated.jpg",
+            "safetyNumber": "Invalid"
+        })
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data["message"], "Safety number must be an 11-digit number")
+
+        # Test case 9: Edit profile with invalid safetyNumber length
+        response = self.app.put('/edit_profile', json={
+            "username": "test_user1",
+            "name": "Updated Name",
+            "age": "25",
+            "email": "updated@email.com",
+            "phone": "12345678901",
+            "address": "Updated Address",
+            "photo": "updated.jpg",
+            "safetyNumber": "123"
+        })
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data["message"], "Safety number must be an 11-digit number")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    unittest.main()
