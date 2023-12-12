326fb6fa273f1dc41844276e7165e357dfbb8f0c
oallace <wallacegsantoslima@gmail.com>
2023-12-12 18:57:21 -0300

    Implements home blueprint

--- backend/app/home/views.py
+++ backend/app/home/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify
+from app.map.views import geocode_address
 import sqlite3
 
 home_bp = Blueprint('home', __name__)
@@ -16,16 +17,20 @@ conn, cursor = setup_home_db()
 # Rotas
 
 
-@home_bp.route('/home/<string:user_id>/safety_number', methods=['GET'])
+@home_bp.route('/home/<string:user_id>/', methods=['GET'])
 def get_home(user_id:  str):
     '''
-    Returns the user's safety number.
+    Returns home info.
 
     :param user_id: User's identification;
     '''
 
+    # Get user info
     consulta = """
-        SELECT 
+        SELECT
+            username,
+            photo,
+            address, 
             safetyNumber
         FROM users
         WHERE id = ?
@@ -38,71 +43,12 @@ def get_home(user_id:  str):
     if user_return is None:
         return jsonify({'error': 'User not found, check the user ID.'}), 404
 
-    safety_number = user_return[0]
-
-    return jsonify({'safety_number': safety_number}), 200
-
-
-@home_bp.route('/home/<string:user_id>/username', methods=['GET'])
-def get_username(user_id:  str):
-    '''
-    Returns the username.
-
-    :param user_id: User's identification;
-    '''
-
-    consulta = """
-        SELECT 
-            username
-        FROM users
-        WHERE id = ?
-    """
-
-    cursor.execute(consulta, (user_id,))
-
-    user_return = cursor.fetchone()
-
-    if user_return is None:
-        return jsonify({'error': 'User not found, check the user ID.'}), 404
-
     username = user_return[0]
+    photo = user_return[1]
+    address = user_return[2]
+    safetyNumber = user_return[3]
 
-    return jsonify({'username': username}), 200
-
-@home_bp.route('/home/<string:user_id>/photo', methods=['GET'])
-def get_photo(user_id:  str):
-    '''
-    Returns the user's photo.
-
-    :param user_id: User's identification;
-    '''
-
-    consulta = """
-        SELECT 
-            photo
-        FROM users
-        WHERE id = ?
-    """
-
-    cursor.execute(consulta, (user_id,))
-
-    user_return = cursor.fetchone()
-
-    if user_return is None:
-        return jsonify({'error': 'User not found, check the user ID.'}), 404
-
-    photo = user_return[0]
-
-    return jsonify({'photo': photo}), 200
-
-@home_bp.route('/home/<string:user_id>/number_of_posts', methods=['GET'])
-def get_number_of_posts(user_id:  str):
-    '''
-    Returns the user's number of posts.
-
-    :param user_id: User's identification;
-    '''
-
+    # Get user number of posts
     consulta = """
         SELECT 
             COUNT(*)
@@ -114,4 +60,55 @@ def get_number_of_posts(user_id:  str):
 
     number_of_posts = cursor.fetchone()[0]
 
-    return jsonify({'number_of_posts': number_of_posts}), 200
+    # Get latitude and longitude
+    location = geocode_address(address)
+
+    # Get posts info
+    # Get user number of posts
+    consulta = """
+        SELECT
+            complaints.id,
+            users.username,
+            users.photo,
+            complaints.description,
+            complaints.likes,
+            complaints.unlikes,
+            complaints.address,
+            complaints.isAnonymous,
+            CASE WHEN likes.user_id IS NOT NULL THEN likes.is_like ELSE NULL END AS user_like
+        FROM complaints
+        JOIN users ON complaints.user_id = users.id
+        LEFT JOIN likes ON complaints.id = likes.complaint_id AND likes.user_id = ?;
+
+    """
+
+    cursor.execute(consulta, (user_id,))
+
+    posts = cursor.fetchall()
+
+    return jsonify({
+        'user': {
+            'username': username,
+            'photo': photo,
+            'address': address,
+            'location': location,
+            'safetyNumber': safetyNumber,
+            'numberOfPosts': number_of_posts
+        },
+
+        'posts':  [
+            {
+                'post_id': p[0],
+                'author_username': p[1],
+                'author_photo': p[2],
+                'post_description': p[3],
+                'likes': p[4],
+                'unlikes': p[5],
+                'address': p[6],
+                'isAnonymous': p[7],
+                'user_like': p[8]
+            }
+            for p in posts
+        ]
+
+    }), 200
